72b5925352a6e3e9ef19fc3f3ea9281ebe69c03e
Alex McNeill <alex.mcneill@gmail.com>
2019-06-12 17:58:40 +0100

    Add Carousel controls (static)

--- src/components/Carousel/index.vue
+++ src/components/Carousel/index.vue
@@ -1,23 +1,37 @@
 <template>
+<div>
   <div class="carousel">
-    <ul v-if="images && images.length">
-      <li :key="image.id" v-for="image of images">
-        <figure>
-          <span>
-            <img
-              :src="image.webformatURL"
-              :alt="image.tags"
-            />
-          </span>
-          <figcaption>{{image.tags}}</figcaption>
-        </figure>
-      </li>
-    </ul>
+    <div class="outer" ref="outer">
+      <ul v-if="images && images.length" ref="inner">
+        <li :key="image.id" v-for="image of images">
+          <figure>
+            <span>
+              <img
+                :src="image.webformatURL"
+                :alt="image.tags"
+              />
+            </span>
+            <figcaption>{{image.tags}}</figcaption>
+          </figure>
+        </li>
+      </ul>
+      <div class="miniControls">
+        <button class="prev"><Arrow /></button>
+        <button class="next"><Arrow /></button>
+      </div>
+    </div>
+
   </div>
+  <div class="controls">
+    <button class="prev">Prev</button>
+    <button class="next">Next</button>
+  </div>
+</div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import Arrow from './arrow.svg';
 
 interface Image {
   id: string;
@@ -25,7 +39,11 @@ interface Image {
   tags: string;
 }
 
-@Component
+@Component({
+  components: {
+    Arrow,
+  },
+})
 export default class Carousel extends Vue {
   @Prop() private images!: Image[];
 }
@@ -33,6 +51,7 @@ export default class Carousel extends Vue {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+  $mobile-button-size: 45px;
   .carousel {
     width: 260px;
     padding: 30px;
@@ -90,12 +109,88 @@ export default class Carousel extends Vue {
     white-space: normal;
     font-size: 1.2em;
     text-transform: capitalize;
-    color: hsl(200, 80%, 40%)
+    color: hsl(200, 80%, 30%)
   }
   img {
     position: relative;
     height: 100%;
     transform: translateX(-50%);
     left: 50%;
+  }
+
+  button {
+    border: 0;
+    cursor: pointer;
+  }
+
+  .outer {
+    position: relative;
+  }
+
+  .miniControls {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+
+    button {
+      position: absolute;
+      top: 30px;
+      width: $mobile-button-size;
+      height: $mobile-button-size * 2;
+      padding: 0;
+      background-color: hsla(200, 80%, 70%, 0.5);
+
+      svg {
+        fill: hsl(200, 80%, 30%);
+        transform: scale(0.75);
+      }
+    }
+
+    .prev {
+      left: 0px;
+      border-bottom-right-radius: $mobile-button-size * 2;
+      border-top-right-radius: $mobile-button-size * 2;
+
+      svg {
+        position: relative;
+        right: 10px;
+        transform: scale(0.75) rotate(180deg);
+      }
+    }
+
+    .next {
+      right: 0px;
+      border-bottom-left-radius: $mobile-button-size * 2;
+      border-top-left-radius: $mobile-button-size * 2;
+
+      svg {
+        position: relative;
+        left: 10px;
+      }
+    }
+  }
+
+  .controls {
+    margin: 20px;
+    text-align: center;
+
+    button {
+      background-color: hsl(200, 80%, 30%);
+      padding: 5px 20px;
+      margin: 3px;
+      color: white;
+      font-weight: bold;
+      font-size: 1.5em;
+
+    }
+    .prev {
+      border-radius: 10px 0 0 10px;
+    }
+
+    .next {
+      border-radius: 0 10px 10px 0;
+    }
   }
 </style>
